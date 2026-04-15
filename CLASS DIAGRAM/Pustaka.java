fa56b0fbbc3f4be19dce5d2960b1866c13863652
Ni Putu Valencia Cinta Kirana <cintakirana267@gmail.com>
2026-04-15 11:39:06 +0800

    Update Pustaka.java

--- CLASS DIAGRAM/Pustaka.java
+++ CLASS DIAGRAM/Pustaka.java
@@ -7,7 +7,6 @@ public class Pustaka {
     int stok;
     String status;
 
-    // CONSTRUCTOR (ini yang bikin error kamu hilang)
     public Pustaka(int idBuku, String judul, String penulis, String penerbit,
                    int tahunTerbit, int stok, String status) {
         this.idBuku = idBuku;
@@ -19,7 +18,6 @@ public class Pustaka {
         this.status = status;
     }
 
-    // METHOD (ini juga wajib ada)
     public void tampilInfo() {
         System.out.println("Judul: " + judul);
         System.out.println("Penulis: " + penulis);
